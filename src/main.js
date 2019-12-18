a48a5fd83d77b4e09fe931c4e73a77026aa84c9f
xuhaofeng <xuhaofeng@163.com>
2019-12-18 16:47:38 +0800

    初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less' // 引入一个基础css样式
 import ElementUI from 'element-ui' // 引入element-ui组件库
 import 'element-ui/lib/theme-chalk/index.css' // 引入 element-ui css 样式
 
