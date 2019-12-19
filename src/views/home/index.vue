678019f4ee279095beb3ca8205dcebff4924e852
xuhaofeng <xuhaofeng@163.com>
2019-12-19 20:09:30 +0800

    主页模块-左侧导航菜单

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,27 +1,23 @@
 <template>
-  <el-row>
-    <el-col :span="6">
-      <el-container>
-        <el-aside>左侧容器</el-aside>
-      </el-container>
-    </el-col>
-    <el-col :span="18">
-      <el-container>
-        <el-header>头部容器</el-header>
-        <el-main>
-          <h6>主要内容</h6>
-          <router-view>二级路由</router-view>
-          </el-main>
-      </el-container>
-    </el-col>
-  </el-row>
+  <el-container>
+    <el-aside style="background-color: #353b4e; height:100vh;width:230px">
+      <layoutAside></layoutAside>
+    </el-aside>
+    <el-container>
+      <el-header>头部容器</el-header>
+      <el-main>
+        <h6>主要内容</h6>
+        <router-view>二级路由</router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import layoutAside from '../../components/layout-aside'
 export default {
-  name: 'home',
-  components: {}
+  components: {
+    layoutAside
+  }
 }
 </script>
