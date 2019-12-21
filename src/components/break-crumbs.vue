6f182b7215c2feb512a42a133ec6fd9ac539c672
xuhaofeng <xuhaofeng@163.com>
2019-12-21 21:54:11 +0800

    评论列表-页面结构-table

--- src/components/break-crumbs.vue
+++ src/components/break-crumbs.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-breadcrumb separator="/">
+  <el-breadcrumb separator=">">
     <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
     <el-breadcrumb-item>
         <slot name='title'></slot>
--- src/views/home/content/comment.vue
+++ src/views/home/content/comment.vue
@@ -1,7 +1,16 @@
 <template>
-  <div>
-      <h1>This is comment</h1>
-  </div>
+  <el-card class="box-card">
+    <breark-crumbs slot="header">
+      <template slot="title">评论列表</template>
+    </breark-crumbs>
+    <el-table :data="tableData" stripe style="width: 100%">
+      <el-table-column prop="" label="标题" width="600"></el-table-column>
+      <el-table-column prop="" label="评论状态"></el-table-column>
+      <el-table-column prop="" label="总评论数"></el-table-column>
+      <el-table-column prop="" label="粉丝评论数"></el-table-column>
+      <el-table-column prop="" label="操作"></el-table-column>
+    </el-table>
+  </el-card>
 </template>
 
 <script>
@@ -11,5 +20,4 @@ export default {
 </script>
 
 <style>
-
 </style>
