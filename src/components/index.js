6044a69ee15cdd1eb4c283693b4555290882006f
xuhaofeng <xuhaofeng@163.com>
2019-12-21 21:02:51 +0800

    主页模块-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeade from './home/layout-heade'
+import Brearkcrumbs from './break-crumbs'
 export default {
   install (Vue) {
     Vue.component('layoutAside', layoutAside)
     Vue.component('layoutHeade', layoutHeade)
+    Vue.component('breark-crumbs', Brearkcrumbs)
   }
 }
