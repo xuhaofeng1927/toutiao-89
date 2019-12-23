1da2e1e081115876ac17eb8a3cf940168527398a
xuhaofeng <xuhaofeng@163.com>
2019-12-24 00:24:46 +0800

    素材列表-分页组件及请求

--- src/views/home/content/material.vue
+++ src/views/home/content/material.vue
@@ -10,17 +10,17 @@
         <div class="img-list">
           <el-card class="img-card" v-for="item in list" :key="item.id">
             <div style="height: 160px;">
-              <img :src="item.url" alt='' />
+              <img :src="item.url" alt />
             </div>
             <el-row class="callget">
               <el-col :span="12">
                 <div class="i">
-                   <i class="el-icon-delete"></i>
+                  <i class="el-icon-delete"></i>
                 </div>
               </el-col>
               <el-col :span="12">
                 <div class="i">
-                 <i class="el-icon-star-off"></i>
+                  <i class="el-icon-star-off"></i>
                 </div>
               </el-col>
             </el-row>
@@ -29,26 +29,38 @@
       </el-tab-pane>
       <el-tab-pane label="收藏素材" name="collect">
         <!-- 收藏素材 -->
-         <div class="img-list">
+        <div class="img-list">
           <el-card class="img-card" v-for="item in list" :key="item.id">
             <div style="height: 160px;">
-              <img :src="item.url" alt='' />
+              <img :src="item.url" alt />
             </div>
             <el-row class="callget">
               <el-col :span="12">
                 <div class="i">
-                   <i class="el-icon-delete"></i>
+                  <i class="el-icon-delete"></i>
                 </div>
               </el-col>
               <el-col :span="12">
                 <div class="i">
-                 <i class="el-icon-star-off"></i>
+                  <i class="el-icon-star-off"></i>
                 </div>
               </el-col>
             </el-row>
           </el-card>
         </div>
-        </el-tab-pane>
+
+      </el-tab-pane>
+      <!-- 分页 -->
+        <el-row type="flex" justify="center" style="height:80px" align="middle">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :total="page.total"
+            :page-sizes="page.pageSize"
+            :current-page="page.currentPage"
+            @current-change="cheagePage"
+          ></el-pagination>
+        </el-row>
     </el-tabs>
   </el-card>
 </template>
@@ -58,21 +70,39 @@ export default {
   data () {
     return {
       activeName: 'all', // 默认选择全部
-      list: [] // 用来获取默认数据
+      list: [], // 用来获取默认数据
+      page: {
+        total: 0, // 默认总条数
+        currentPage: 1, // 默认当前页数，第一条页数
+        pageSize: 10 // 默认每页条数,好像默认就是10
+      }
     }
   },
   methods: {
+    // 获取图片数据
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: this.activeName === 'collect'
+          collect: this.activeName === 'collect', // 判断boolern值来确定all或者collect
+          page: this.page.currentPage,
+          per_page: this.page.pageSize
         }
       }).then(result => {
+        // 获取图片的数据
         this.list = result.data.results
+        // 获取图片的总页数
+        this.page.total = result.data.total_count
       })
     },
+    // 切换事件
     handleClick () {
+      this.page.currentPage = 1 // 切换默认当前页数为1
+      this.getMaterial()
+    },
+    //  改变页数事件
+    cheagePage (newPage) {
+      this.page.currentPage = newPage
       this.getMaterial()
     }
   },
@@ -97,7 +127,7 @@ export default {
     .callget {
       width: 100%;
       height: 30px;
-      margin-top:10px;
+      margin-top: 10px;
       .grid-content {
         border-radius: 4px;
         min-height: 36px;
