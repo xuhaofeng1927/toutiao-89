98f8d82b00071c375b212d26bdb9d89dca33d0ee
xuhaofeng <xuhaofeng@163.com>
2019-12-26 18:28:20 +0800

    发表文章-发布文章检验逻辑实现

--- src/views/home/content/publish.vue
+++ src/views/home/content/publish.vue
@@ -4,22 +4,22 @@
       <template slot="title">发表内容</template>
     </breark-crumbs>
     <!-- 1，快速构建表单结构 -->
-    <el-form :label-position="labelPosition" label-width="80px" :model="publishForm">
-      <el-form-item label="文章标题">
-        <el-input v-model="publishForm.name"></el-input>
+    <el-form label-position="right" ref='articlesForm' label-width="100px" :model="publishForm" :rules=" publishrules">
+      <el-form-item label="文章标题" prop="title">
+        <el-input v-model="publishForm.title"></el-input>
       </el-form-item>
-      <el-form-item label="文章内容">
-        <el-input v-model="publishForm.region"></el-input>
+      <el-form-item label="文章内容" prop='content'>
+        <el-input v-model="publishForm.content"></el-input>
       </el-form-item>
-      <el-form-item label="文章封面">
-        <el-radio-group v-model="publishForm.radio">
+      <el-form-item label="文章封面" prop="type">
+        <el-radio-group v-model="publishForm.cover.type">
           <el-radio :label="-1">自动</el-radio>
           <el-radio :label="0">无图</el-radio>
           <el-radio :label="1">单图</el-radio>
           <el-radio :label="3">三图</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道类型">
+      <el-form-item label="频道类型" prop="channel_id">
          <el-select v-model="publishForm.channel_id" placeholder="请选择">
             <el-option
               v-for="item in channelOptions"
@@ -30,8 +30,8 @@
           </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发布</el-button>
-        <el-button plain>存入草稿</el-button>
+        <el-button type="primary" @click='publishAticle'>发布</el-button>
+        <el-button plain @click='publishAticle'>存入草稿</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -41,16 +41,32 @@
 export default {
   data () {
     return {
-      labelPosition: 'right',
+      // 表单内容数据
       publishForm: {
-        name: '',
-        region: '',
-        type: '',
-        radio: -1,
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: -1, // 图片状态
+          images: [] // 图片信息
+        },
         channel_id: null // 频道列表
       },
-      channelOptions: [],
-      value: ''
+      //   表单校验规则
+      publishrules: {
+        title: [
+          { required: true, message: '请输入文章标题', trigger: 'blur' },
+          { min: 5, message: '内容不少于五个字', trigger: 'blur' }, // 最小字段规则
+          { max: 30, message: '内容以达到最大值', trigger: 'blur' } // 最大字段规则
+        ],
+        content: [
+          { required: true, message: '请输入文章内容', trigger: 'blur' }
+        ],
+        type: [],
+        channel_id: [
+          { required: true, message: '频道列表不能为空' }
+        ]
+      },
+      channelOptions: [] // 接收频道数据
     }
   },
   methods: {
@@ -60,6 +76,14 @@ export default {
         url: '/channels'
       }).then(result => {
         this.channelOptions = result.data.channels
+      })
+    },
+    // 3，全局校验以及添加内容
+    publishAticle () {
+      this.$refs.articlesForm.validate((isOk) => {
+        if (isOk) {
+          alert('校验成功')
+        }
       })
     }
   },
