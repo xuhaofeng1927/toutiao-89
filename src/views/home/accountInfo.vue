3105ee4b75797629aa2448cafe17c39f1bba9dfe
xuhaofeng <xuhaofeng@163.com>
2019-12-28 22:10:20 +0800

    账户信息-用户数据保存

--- src/views/home/accountInfo.vue
+++ src/views/home/accountInfo.vue
@@ -8,22 +8,26 @@
       <img :src="accountInfoForm.photo?accountInfoForm.photo:defaultphoto" alt />
     </el-upload>
     <el-form class="accountInfoForm"
+    ref="myInfoForm"
       label-position="left"
       label-width="80px"
       :model="accountInfoForm"
       :rules="InfoFormRule"
     >
-      <el-form-item label="用户名">
+      <el-form-item label="用户名" prop="name">
         <el-input v-model="accountInfoForm.name"></el-input>
       </el-form-item>
-      <el-form-item label="个人签名">
+      <el-form-item label="个人签名" prop='intro'>
         <el-input v-model="accountInfoForm.intro"></el-input>
       </el-form-item>
-      <el-form-item label="邮件">
+      <el-form-item label="邮件" prop="email">
         <el-input v-model="accountInfoForm.email"></el-input>
       </el-form-item>
-      <el-form-item label="手机号">
+      <el-form-item label="手机号" prop="mobile">
         <el-input v-model="accountInfoForm.mobile" disabled></el-input>
+      </el-form-item>
+       <el-form-item>
+        <el-button type="primary" @click="submitAccountForm">保存信息</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -42,16 +46,45 @@ export default {
         photo: '' // 头像
       },
       InfoFormRule: {
-        name: [], // 用户名
-        intro: [], // 个人签名
-        email: [], // 邮箱
+        name: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 2, max: 7, message: '用户名最少为两个字节' }
+        ], // 用户名
+        intro: [
+          { required: true, message: '请输入内容', trigger: 'blur' }
+        ], // 个人签名
+        email: [
+          { required: true,
+            pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+            message: '邮箱格式不正确',
+            trigger: 'blur' }
+        ], // 邮箱
         mobile: [], // 手机号
         photo: [] // 头像
       }
     }
   },
   methods: {
-    // 获取个人账户信息
+    // 2,校验表单信息提交
+    submitAccountForm () {
+      this.$refs.myInfoForm.validate(isOk => {
+        if (isOk) {
+          // alert('校验成功')
+          this.$axios({
+            url: '/user/profile',
+            method: 'patch',
+            data: this.accountInfoForm
+          }).then(() => {
+            this.$message({
+              type: 'success',
+              message: '保存成功'
+            })
+            this.getAccountInfo()
+          })
+        }
+      })
+    },
+    // 1,获取个人账户信息
     getAccountInfo () {
       this.$axios({
         url: '/user/profile'
