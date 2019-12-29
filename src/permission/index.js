8563df9de64028cb7ba72a26fc179b2376b5c228
xuhaofeng <xuhaofeng@163.com>
2019-12-29 20:55:01 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,6 +1,11 @@
 // 权限拦截，导航守卫
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  // 添加导航进度
+  progress.start()
+
   // 拦截谁判断拦截地址
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
@@ -12,4 +17,8 @@ router.beforeEach(function (to, from, next) {
   } else {
     next() // 直接放行
   }
+})
+router.afterEach(function () {
+  // progress.done()
+  setTimeout(() => { progress.done() }, 1000)
 })
